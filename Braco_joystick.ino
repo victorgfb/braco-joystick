dd40ec7c649681c4203b17ca68cfbc8950c1cedf
Victor <victorgfb80@gmail.com>
2018-01-22 00:15:56 -0200

    Commit

---
+++ Braco_joystick.ino
@@ -0,0 +1,53 @@
+/*
+*  Projeto:		Calibrar JoyStick
+*
+*  Objetivo:	Encontrar regia�o de inconsist�ncia nas leituras dos zeros de JoySticks
+*
+*  Obs.:
+*
+*  Autor:		Lucas Dami�o  - Estudante de Eng. El�trica - UNIVASF
+*
+*  Criado:		08/11/17      Mod.: dd/mm/aa
+*
+*/
+
+/* --- Bibliotecas --- */
+#include <Arm.h>
+
+/* --- Defini��es --- */
+
+
+/* --- Classes --- */
+
+/* --- Fun��es Adicionais --- */
+
+/* --- Objetos --- */
+Arm braco;
+
+/* --- Vari�veis Globais --- */
+const byte	numServos = 6,
+			joysticks[] = { A0, A1, A2, A3, A4, A5 },
+			hand = 6;
+     
+int   pinServos[] = { 3, 5, 6, 9, 10, 11 },
+      zeros[] = { 10, 20, 20, 20, 20, 20 };
+      
+
+
+
+void setup()
+{
+	Serial.begin(9600);
+	braco.config(pinServos, numServos, zeros, hand - 1);
+  braco.invertServo(1); 
+}
+
+void loop()
+{
+	braco.Run(3);
+	
+	for (int i = 0; i < numServos; i++)
+		Serial.print(String(braco.getPosicao(i)) + "\t");
+	Serial.println();
+	delay(60);
+}
---
+++ Debug/Braco_joystick.cpp.d
@@ -0,0 +1,16 @@
+C:\Users\DamLucas\AppData\Local\Temp\VMBuilds\Braco_joystick\mega_atmega2560\Debug\Braco_joystick.cpp.o: \
+ C:\Users\DamLucas\AppData\Local\Temp\VMBuilds\Braco_joystick\mega_atmega2560\Debug\Braco_joystick.cpp \
+ D:\OneDrive\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ D:\OneDrive\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ D:\OneDrive\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ D:\OneDrive\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ D:\OneDrive\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ D:\OneDrive\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ D:\OneDrive\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ D:\OneDrive\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ D:\OneDrive\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ D:\OneDrive\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ D:\OneDrive\Arduino\hardware\arduino\avr\variants\mega/pins_arduino.h \
+ D:\OneDrive\Arduino\libraries\Arm\src/Arm.h \
+ D:\OneDrive\Arduino\libraries\Arm\src/avr/ServoTimers.h \
+ C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\ 14.0\Common7\IDE\Extensions\dhz2zcju.sv0\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h
